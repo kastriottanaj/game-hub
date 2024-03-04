0edad359a651da0cc7f3c438690594927726f4ca
Led-Com.CH <led-com.ch@airvonledcomch.home>
2024-03-04 17:02:14 +0100

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwtich";
+import SearchInput from "./Searchinput";
 
 const NavBar = () => {
     return (
-        <HStack justifyContent="space-between" padding="10px">
+        <HStack padding="10px">
             <Image src={logo} boxSize="60px" />
-            <h1>Kastriot a.k.a Toti</h1>
+            <SearchInput />
             <ColorModeSwitch />
         </HStack>
     )
